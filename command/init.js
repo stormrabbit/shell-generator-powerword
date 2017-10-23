84625ded1c9f94108892a1729ed9760b1bd51dbb
欧阳夏昱 <ouyangxiayu@m24.cn>
2017-10-23 23:07:10 +0800

    fix bug

--- command/init.js
+++ command/init.js
@@ -6,8 +6,19 @@ const exec = require('child_process').exec
 
 module.exports = () => {
   co(function*(){
-    let name = yield prompt('input your code:\n')
-    console.log('name==>', name)
-    process.exit()
+
+    let name = yield prompt('Project name: ')
+    let cmdStr = `git clone https://github.com/stormrabbit/spell-book-powerword.git ${name} && cd ${name} && git checkout master`
+    console.log(chalk.white('\n start generating...'))
+    
+    exec(cmdStr, (error, stdout, stderr) => {
+      if (error) {
+        console.log(error)
+        process.exit()
+      }
+      console.log(chalk.green('\n √ Generation completed!'))
+      console.log(`\n cd ${name} && npm install \n`)
+      process.exit()
+    })
   })
 }
